70e0ad4e98a42a7e38dc8699c546d0d076a79a9d
dxlliv <hello@dxlliv.dev>
2025-05-19 01:32:01 +0200

    fix: Avoid jetstream connection if watched user is the logged user

--- runtime/plugin.ts
+++ runtime/plugin.ts
@@ -1,4 +1,5 @@
 import { defineNuxtPlugin, useRuntimeConfig } from 'nuxt/app'
+import { useAtproto, useAgent } from '#imports'
 import { startActorDesktopStreamw } from './utils/utilAtprotoApplicationStates'
 
 export default defineNuxtPlugin({
@@ -7,11 +8,21 @@ export default defineNuxtPlugin({
   async setup(nuxt) {
     nuxt.hook('app:mounted', () => {
       const runtimeConfig = useRuntimeConfig()
+      const atproto = useAtproto()
 
       if (
         runtimeConfig.public.atprotoJetstream &&
         runtimeConfig.public.atprotoJetstream.startOwnerDesktopStreamOnMounted
       ) {
+
+        if (atproto.isLogged()) {
+          const agent = useAgent('private')
+
+          if (runtimeConfig.public.atprotoDesktop.owner.did === agent.assertDid) {
+            return
+          }
+        }
+
         startActorDesktopStreamw(runtimeConfig.public.atprotoDesktop.owner.did)
       }
     })
